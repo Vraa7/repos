9a679487b969a5e06606c2ae7d0e15ed5abadbfb
Vraa7 <uwais2130@gmail.com>
2025-06-04 10:41:44 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "";
+$pass = ""; 
 $dbname = "ctf_web";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Koneksi database gagal: " . $conn->connect_error);
 }
 ?>
